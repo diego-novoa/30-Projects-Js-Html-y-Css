00b50d69f6ba388923dfab6892819d8259949955
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-05-28 19:27:12 -0500

    ok

--- Heart Trail Animation/index.js
+++ Heart Trail Animation/index.js
@@ -1,9 +1,15 @@
-const bodyElemet = document.querySelector("body")
+const bodyElemet = document.querySelector("body");
 bodyElemet.addEventListener("mausemove",(event)=>{
-    const xPos = event.offsetX;
-    const yPos = event.offsety;
-    const spanElemet = document.createElement("span");
-    spanElemet.style.left = xPos + "px";
-    spanElemet.style.top = yPos + "px";
-    bodyElemet.appendChild("spanElemet");
-})
+  const xPos = event.offsetX;
+  const yPos = event.offsety;
+  const spanElemet = document.createElement("span");
+  spanElemet.style.left = xPos + "px";
+  spanElemet.style.top = yPos + "px";
+	const size = Math.random() *100;
+	spanElemet.style.width = size + "px";
+	spanElemet.style.height = size + "px";
+  bodyElemet.appendChild("spanElemet");
+  setTimeout(()=>{
+		spanElemet.remove();
+  }, 3000);
+});
