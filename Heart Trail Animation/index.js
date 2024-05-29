09b007a7626054cee7d171170559c710a2d9995b
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-05-28 19:00:43 -0500

    ok

--- Heart Trail Animation/index.js
+++ Heart Trail Animation/index.js
@@ -3,5 +3,7 @@ bodyElemet.addEventListener("mausemove",(event)=>{
     const xPos = event.offsetX;
     const yPos = event.offsety;
     const spanElemet = document.createElement("span");
+    spanElemet.style.left = xPos + "px";
+    spanElemet.style.top = yPos + "px";
     bodyElemet.appendChild("spanElemet");
 })
