4d8e96e674fffc6bbe078c8925ab4d1aea805311
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-05-31 16:49:06 -0500

    ok

---
+++ Preview-Dark mode toggle/index.js
@@ -0,0 +1,20 @@
+const inputElement = document.querySelector(".input");
+const bodyElement = document.querySelector("body");
+
+inputElement.checked = JSON.parse(localStorage.getItem("mode"));
+updateBody();
+
+function updateBody(){
+  if(inputElement.checked){
+    bodyElement.style.background = "black";
+  }else{
+    bodyElement.style.background = "white";
+  }
+}
+inputElement.addEventListener("input",()=>{
+	updateBody()
+})
+
+function updateLocalStorage(){
+	localStorage.setItem("mode",JSON.stringify(inputElement.checked))
+}
