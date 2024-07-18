b2e3842ac7f4a2b99846db72bf4644fd25de5506
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-07-17 22:11:13 -0500

    ok

--- Random- Color Generator/index.js
+++ Random- Color Generator/index.js
@@ -1,1 +1,27 @@
-let 
+const containerElement = document.querySelector(".container");
+
+for (let index = 0; index < 30; index++) {
+  const containerElement = document.createElement("div");
+  containerElement.classList.add("color-container");
+  containerElement.appendChild(containerElement);
+}
+
+const colorContainerElement = document.querySelectorAll(".color-container");
+function generaterColors(){
+  colorContainerElement.forEach((colorContainerElement)=>{
+    const newColorCode = randomColor()
+    colorContainerElement.style.backgrouncolor = "#" + newColorCode;
+    colorContainerElement.innerText = "#" + newColorCode;
+  })  
+}
+
+function randomColor(){
+  const chars = "0123456789abcdf"
+  const colorCodeLehgth = 6;
+  let colorCode = "";
+  for (let index = 0; index < colorCodeLehgth; index++) {
+    const randomNum = Math.floor(Math.random()*chars.length);
+    colorCode += chars.substring(randomNum,randomNum + 1);
+  }
+  return colorCode;
+}
