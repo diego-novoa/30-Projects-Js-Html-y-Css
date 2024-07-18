920d482a493c3fad16c16f3b14cd177ad3ab1e9d
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-07-17 22:14:08 -0500

    ok

--- Random- Color Generator/index.js
+++ Random- Color Generator/index.js
@@ -1,27 +1,30 @@
-const containerElement = document.querySelector(".container");
+const containerEl = document.querySelector(".container");
 
 for (let index = 0; index < 30; index++) {
-  const containerElement = document.createElement("div");
-  containerElement.classList.add("color-container");
-  containerElement.appendChild(containerElement);
+  const colorContainerEl = document.createElement("div");
+  colorContainerEl.classList.add("color-container");
+  containerEl.appendChild(colorContainerEl);
 }
 
-const colorContainerElement = document.querySelectorAll(".color-container");
-function generaterColors(){
-  colorContainerElement.forEach((colorContainerElement)=>{
-    const newColorCode = randomColor()
-    colorContainerElement.style.backgrouncolor = "#" + newColorCode;
-    colorContainerElement.innerText = "#" + newColorCode;
-  })  
+const colorContainerEls = document.querySelectorAll(".color-container");
+
+generateColors();
+
+function generateColors() {
+  colorContainerEls.forEach((colorContainerEl) => {
+    const newColorCode = randomColor();
+    colorContainerEl.style.backgroundColor = "#" + newColorCode;
+    colorContainerEl.innerText = "#" + newColorCode;
+  });
 }
 
-function randomColor(){
-  const chars = "0123456789abcdf"
-  const colorCodeLehgth = 6;
+function randomColor() {
+  const chars = "0123456789abcdef";
+  const colorCodeLength = 6;
   let colorCode = "";
-  for (let index = 0; index < colorCodeLehgth; index++) {
-    const randomNum = Math.floor(Math.random()*chars.length);
-    colorCode += chars.substring(randomNum,randomNum + 1);
+  for (let index = 0; index < colorCodeLength; index++) {
+    const randomNum = Math.floor(Math.random() * chars.length);
+    colorCode += chars.substring(randomNum, randomNum + 1);
   }
   return colorCode;
 }
