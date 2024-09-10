9116f9cf5f4818e1093bc95ddef2b3649cfe8ae5
Diego Novoa <[200~64092270+diego-novoa@users.noreply.github.com>
2024-09-09 21:40:36 -0500

    pl

--- 19- Video Trailer Popup/index.js
+++ 19- Video Trailer Popup/index.js
@@ -1,3 +1,14 @@
 const btnEl = document.querySelector(".btn");
 const CloseIconEl = document.querySelector(".Close-icon");
 const trailerContainerEl = document.querySelector(".trailer-container");
+const videoEl = document.querySelector("video")
+
+btnEl.addEventListener("click", ()=>{
+  trailerContainerEl.classList.remove("active");
+});
+
+CloseIconEl.addEventListener("click", () => {
+  trailerContainerEl.classList.add("active");
+  videoEl.pause();
+  videoEl.currentTime = 0;
+});
